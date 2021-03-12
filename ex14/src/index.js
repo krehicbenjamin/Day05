683b4eeb9de8894ec4afc7e5339c0e9dfa5972e6
krehicbenjamin <krehicbenjamin@gmail.com>
2021-03-12 22:49:03 +0100

    adding all the tasks for day 5

---
+++ ex14/src/index.js
@@ -0,0 +1,24 @@
+function main() {
+  //Only change code below this line
+  var myNoun = "dog";
+  var myVerb = "ran";
+  var myAdjective = "big";
+  var myAdverb = "quickly";
+  var wordBlanks =
+    "My " +
+    myNoun +
+    " " +
+    myVerb +
+    " " +
+    myAdverb +
+    " when he saw the " +
+    myAdjective +
+    " bowl of food.";
+  //Only change code above this line
+
+  return wordBlanks;
+}
+
+console.log(main());
+
+module.exports = main;
